aab00eaa45803783dca3460192fe3fea528cced6
jabez007 <jabez007@users.noreply.github.com>
2024-11-14 14:27:27 -0600

    simplify container entrypoint

--- astro-nvim/entrypoint.sh
+++ astro-nvim/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Load nvm
-source ~/.nvm/nvm.sh
 
 # Run nvim with passed arguments or open the current directory by default
 if [ "$#" -eq 0 ]; then
-  nvim .
+  nvim
 else
   nvim "$@"
 fi
